c215b5f9bdbe988cb2822fa453a886d46cb80ce0
Shea Hawkins <shea.m.hawkins@gmail.com>
2016-04-17 15:11:16 -0600

    Implement last function.

--- src/underbar.js
+++ src/underbar.js
@@ -38,6 +38,12 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n === 0) {
+      return [];
+    } else if (n > array.length) {
+      return array;
+    }
+    return n === undefined ? array[array.length - 1] : array.slice(n - 1, array.length);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -46,6 +52,7 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
